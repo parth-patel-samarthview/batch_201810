1db0cab11c3650b1fa4959adf22fde1dabd7695d
parthpatel <samarthview@gmail.com>
2018-10-21 13:27:26 +0530

    This is initial commit

---
+++ class_01.py
@@ -0,0 +1,11 @@
+"""
+This is class 01 file
+
+1) we set up python3.7, git, pycharm
+2) created github repo for batch
+3) created virtual environment
+4) setup pycharm project
+5) created this file and greeted entire world.. :)
+
+"""
+print("Hello World")
